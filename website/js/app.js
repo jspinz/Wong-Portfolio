f9c33a537757146b27a69b506223024fc33b7608
Jason Wong <jkw325@nyu.edu>
2017-03-20 13:28:53 -0400

    Revert "Revert "dj website live""
    This reverts commit 6951d490106b0eca2178cc4ca77fee82421823d6.

---
+++ website/js/app.js
@@ -0,0 +1,49 @@
+//angular
+//    .module('app', [
+//    'ui.router'
+//  ])
+//    .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+//        $urlRouterProvider.otherwise('/');
+//        $stateProvider
+//            .state('home', {
+//                url: '',
+//                templateUrl: 'index.html',
+//            })
+//            .state('weddings', {
+//                url: ./'weddings',
+//                templateUrl: 'weddings.html',
+//            })
+//            .state('fashion-retail', {
+//                url: ./'fashion-retail',
+//                templateUrl: 'fashion-retail.html'
+//            })
+//            .state('press', {
+//                url: '/press',
+//                templateUrl: 'partials/press.html'
+//            })
+//            .state('contact', {
+//                url: '/contact',
+//                templateUrl: 'partials/contact.html'
+//            })
+//            .state('gallery', {
+//                url: '/gallery',
+//                templateUrl: 'partials/gallery.html'
+//            })
+//  }]);
+
+//var app = angular.module("app", ["ngRoute"]);
+//app.config(function ($routeProvider) {
+//    $routeProvider
+//        .when("/", {
+//            templateUrl: "index.html"
+//        })
+//        .when("/weddings", {
+//            templateUrl: "weddings.html"
+//        })
+//        .when("/green", {
+//            templateUrl: "green.htm"
+//        })
+//        .when("/blue", {
+//            templateUrl: "blue.htm"
+//        });
+//});
